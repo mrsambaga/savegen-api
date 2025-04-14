f9699db8f6724f846dc9b74dd07ee2703fd7e249
mrsambaga <m.r.sambaga@gmail.com>
2025-04-14 22:46:12 +0700

    create postgresql config

---
+++ config/database.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "bookstore"
+)
+
+func GetDB() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected to PostgreSQL database!")
+	return db
+} 
