fb15cd505158ebdb02c60c2cf8523469bff4ccd4
mrsambaga <m.r.sambaga@gmail.com>
2025-04-18 18:54:10 +0700

    rearange db migrate up

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -1,14 +1,14 @@
--- Create transaction_types table
-CREATE TABLE IF NOT EXISTS transaction_types (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
 -- Create users table
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create transaction_types table
+CREATE TABLE IF NOT EXISTS transaction_types (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
