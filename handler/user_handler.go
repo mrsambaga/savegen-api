dc290d2c9d7b64f76744051aa34702f9ab75b14f
mrsambaga <m.r.sambaga@gmail.com>
2025-07-06 21:53:05 +0700

    add email validation in get user api

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -2,12 +2,15 @@ package handler
 
 import (
 	"net/http"
+	"regexp"
 	"savegen-api/dto"
 	"savegen-api/usecase"
 	"savegen-api/util"
 
 	"github.com/gin-gonic/gin"
 )
+
+var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}`);
 
 type UserHandler struct {
 	userUsecase usecase.UserUsecase
@@ -39,6 +42,24 @@ func (h *Handler) CreateUser(c *gin.Context) {
 func (h *Handler) GetUserByEmail(c *gin.Context) {
 	email := c.Param("email")
 
+	if email == "" {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code":     "BAD_REQUEST",
+			"messages": "Email is mandatory",
+			"data":     nil,
+		})
+		return
+	}
+
+	if (!isValidEmail(email)) {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code":     "BAD_REQUEST",
+			"messages": "Email is invalid",
+			"data":     nil,
+		})
+		return
+	}
+
 	user, err := h.userUsecase.GetUserByEmail(email)
 	if err != nil {
 		util.RespondWithError(c, err)
@@ -53,5 +74,12 @@ func (h *Handler) GetUserByEmail(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, response)
+}
+
+func isValidEmail(email string) bool {
+	if len(email) == 0 || len(email) > 254 {
+		return false
+	}
+	return emailRegex.MatchString(email)
 }
 
